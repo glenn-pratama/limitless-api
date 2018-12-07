20b31fcbaed38a7982a569d93db1051ac924e6e9
unknown <glenn.pratama@gmail.com>
2018-12-07 15:24:09 +0700

    Initial structure for API

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = require('./router');
+const app = express();
+const port = 4000;
+
+app.use('/', router);
+app.listen(port, () => console.log(`Limitless API listening on port ${port}!`));
