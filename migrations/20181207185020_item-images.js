4065885b5ed00d4f850e1792cb51bc61abfeca50
unknown <glenn.pratama@gmail.com>
2018-12-07 18:59:52 +0700

    Added item images and prices migration script

---
+++ migrations/20181207185020_item-images.js
@@ -0,0 +1,11 @@
+exports.up = async (knex) => {
+  return knex.schema.createTable('item_images', t => {
+    t.increments();
+    t.integer('item_url_id').references('id').inTable('item_urls');
+    t.text('url').notNullable();
+    t.boolean('is_active').notNullable().defaultTo(true);
+    t.timestamps(false, true);
+  });
+};
+
+exports.down = async (knex) => knex.schema.dropTableIfExists('item_images');
---
+++ migrations/20181207185635_item-prices.js
@@ -0,0 +1,13 @@
+exports.up = async (knex) => {
+  return knex.schema.createTable('item_prices', t => {
+    t.increments();
+    t.integer('item_url_id').references('id').inTable('item_urls');
+    t.string('currency').notNullable();
+    t.bigInteger('normal_price').notNullable();
+    t.bigInteger('discounted_price').notNullable();
+    t.string('status').notNullable();
+    t.timestamps(false, true);
+  });
+};
+
+exports.down = async (knex) => knex.schema.dropTableIfExists('item_prices');
