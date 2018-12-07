4d9c17eb02fc9e88e084c0daf76cc9e4612eeef9
unknown <glenn.pratama@gmail.com>
2018-12-07 18:19:20 +0700

    Added knex and base config structure

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+const config = require('config');
+const dbConfig = config.get('General.dbConfig');
+
+module.exports = {
+  client: dbConfig.client,
+  connection: dbConfig.connection,
+  pool: dbConfig.pool,
+};
--- src/app.js
+++ src/app.js
@@ -5,4 +5,7 @@ const app = express();
 const port = 4000;
 
 app.use('/', router);
-app.listen(port, () => console.log(`Limitless API listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Limitless API listening on port ${port}`);
+  console.log(`Environment used is ${process.env.NODE_ENV}`);
+});
