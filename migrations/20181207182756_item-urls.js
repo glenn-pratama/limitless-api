b790781e9d3710a82a26cd38159db7e8013bb556
unknown <glenn.pratama@gmail.com>
2018-12-07 18:50:49 +0700

    Added item urls migration script

---
+++ migrations/20181207182756_item-urls.js
@@ -0,0 +1,14 @@
+exports.up = async (knex) => {
+  return knex.schema.createTable('item_urls', t => {
+    t.increments();
+    t.uuid('item_id').references('id').inTable('items');
+    t.string('source_type').notNullable();
+    t.string('name').notNullable();
+    t.text('description');
+    t.integer('monitor_frequency').notNullable().defaultTo(1);
+    t.boolean('is_active').notNullable().defaultTo(true);
+    t.timestamps(false, true);
+  });
+};
+
+exports.down = async (knex) => knex.schema.dropTableIfExists('item_urls');
