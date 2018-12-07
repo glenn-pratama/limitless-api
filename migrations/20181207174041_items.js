84c732e0d7e45604ef347275f2eae5fe9c775ded
unknown <glenn.pratama@gmail.com>
2018-12-07 18:19:38 +0700

    Added items table migration script

---
+++ migrations/20181207174041_items.js
@@ -0,0 +1,16 @@
+exports.up = async knex => {
+  await knex.raw('create extension if not exists "uuid-ossp"');
+
+  return knex.schema.createTable('items', t => {
+    t.uuid('id').notNullable().defaultTo(knex.raw('uuid_generate_v4()')).primary();
+    t.string('name').notNullable();
+    t.text('description');
+    t.boolean('is_active').notNullable().defaultTo(true);
+    t.timestamps(false, true);
+  });
+};
+
+exports.down = async knex => {
+  await knex.schema.dropTableIfExists('items');
+  return knex.raw('drop extension if exists "uuid-ossp"');
+};
