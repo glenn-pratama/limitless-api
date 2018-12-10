649b8f46f393abcd881ec8b02f98ddc997f53efd
unknown <glenn.pratama@gmail.com>
2018-12-11 05:03:33 +0700

    Added last run indicator

--- migrations/20181207182756_item-urls.js
+++ migrations/20181207182756_item-urls.js
@@ -1,14 +1,13 @@
-exports.up = async (knex) => {
-  return knex.schema.createTable('item_urls', (t) => {
-    t.increments();
-    t.uuid('item_id').references('id').inTable('items');
-    t.string('source_type').notNullable();
-    t.text('url').notNullable();
-    t.text('description');
-    t.integer('monitor_frequency').notNullable().defaultTo(1);
-    t.boolean('is_active').notNullable().defaultTo(true);
-    t.timestamps(false, true);
-  });
-};
+exports.up = async knex => knex.schema.createTable('item_urls', (t) => {
+  t.increments();
+  t.uuid('item_id').references('id').inTable('items');
+  t.string('source_type').notNullable();
+  t.text('url').notNullable();
+  t.text('description');
+  t.integer('monitor_frequency').notNullable().defaultTo(1);
+  t.datetime('last_run').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+  t.boolean('is_active').notNullable().defaultTo(true);
+  t.timestamps(false, true);
+});
 
 exports.down = async knex => knex.schema.dropTableIfExists('item_urls');
